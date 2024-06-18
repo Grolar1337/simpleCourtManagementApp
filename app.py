f29cc1207c3fd060e8150ae005f4e3da77ac4b2b
Grolar1337 <blkyakupsait@gmail.com>
2024-06-18 12:01:50 +0300

    a

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from sanic import Sanic
+from sanic import Sanic, Blueprint
 from sanic.log import logger
 from tortoise.contrib.sanic import register_tortoise
 import random
 import string
 
+from user import user
+from schedule import schedule
 from models import *
 
 app= Sanic("app")
@@ -14,6 +16,11 @@ app.config.ADMIN_CODE= ''.join(random.choices(string.ascii_uppercase + string.di
 logger.info('Here is your admin code: '+app.config.ADMIN_CODE)
 #https://sanic.dev/en/guide/running/configuration.html#loading
 
+
+api_group= Blueprint.group(
+    user, schedule, url_prefix="/api")
+
+app.blueprint(api_group)
 
 register_tortoise(
     app, db_url="sqlite://database/database.sqlite3", modules={"models": ["models"]}, generate_schemas=True
